1ce12b7b72d48bd89b68abda346a16cded0646e2
ana004 <paulaana08002019@gmail.com>
2022-05-17 15:27:59 -0300

    refactor: add computer repository

---
+++ Repositories/ComputerRepository.cs
@@ -0,0 +1,33 @@
+using LabManager.Database;
+using LabManager.Models;
+using Microsoft.Data.Sqlite;
+
+namespace LabManager.Repositories;
+
+class ComputerRepository
+{
+    private DatabaseConfig databaseConfig;
+
+    public ComputerRepository(DatabaseConfig databaseConfig) => this.databaseConfig = databaseConfig;
+
+    public List<Computer> GetAll()
+    {
+        var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        connection.Open();
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Computers;";
+
+        var reader = command.ExecuteReader();
+
+        var computers = new List<Computer>();
+
+        while(reader.Read())
+        {
+            computers.Add(new Computer(reader.GetInt32(0), reader.GetString(1), reader.GetString(2)));
+        }
+            
+        connection.Close();
+        
+        return computers;
+    }
+}
