32fcd31d64fadbaaa99b0fe2a47da59064fe5b1d
ana004 <paulaana08002019@gmail.com>
2022-05-17 00:44:02 -0300

    Adicionados os comandos Show, Update e Delete

--- Program.cs
+++ Program.cs
@@ -62,4 +62,74 @@ if (modelName == "Computer")
 
         connection.Close();
     }
+
+    if (modelAction == "Show")
+    {
+       connection = new SqliteConnection("Data Source=database.db");
+       connection.Open(); 
+       int id = Convert.ToInt32(args[2]);
+       command = connection.CreateCommand();
+       command.CommandText = "SELECT * FROM Computers WHERE id=5";
+       
+       var reader = command.ExecuteReader();
+
+
+       while(reader.Read())
+       {
+           if (reader.GetInt32(0) == id)
+           Console.WriteLine("{0}, {1}, {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+       }
+
+       reader.Close();
+       connection.Close();
+    }
+
+    if (modelAction == "Update")
+    {
+       connection = new SqliteConnection("Data Source=database.db");
+       connection.Open(); 
+       int id = Convert.ToInt32(args[2]);
+       string ram = args[3];
+       string processor = args[4];
+       command = connection.CreateCommand();
+       command.CommandText = "UPDATE Computers SET id=5, ram='2', processor='Intel Core' WHERE id=5";
+
+       var reader = command.ExecuteReader();
+
+
+       while(reader.Read())
+       {
+           if (reader.GetInt32(0) == id)
+           { 
+           command.Parameters.AddWithValue("$id", id);
+        command.Parameters.AddWithValue("$ram", ram);
+        command.Parameters.AddWithValue("$processor", processor);}
+       }
+
+       reader.Close();
+       connection.Close();
+    }
+
+    if (modelAction == "Delete")
+    {
+        connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+        int id = Convert.ToInt32(args[2]);
+        command = connection.CreateCommand();
+        command.CommandText = "DELETE FROM Computers WHERE id=4";
+        
+       var reader = command.ExecuteReader();
+
+
+       while(reader.Read())
+       {
+           if (reader.GetInt32(0) == id)
+           command.Parameters.RemoveAt("@id");
+        command.Parameters.RemoveAt("@ram");
+        command.Parameters.RemoveAt("@processor");
+       }
+
+       reader.Close();
+       connection.Close();
+    }
 }
